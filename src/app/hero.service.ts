542436d49bba3198c5fd54d8627a9f5dd9f4958e
Nicolas Ruiz <syngenruiz2@gmail.com>
2021-11-01 04:40:04 -0500

    Finished for now

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -79,4 +79,28 @@ export class HeroService {
       catchError(this.handleError<Hero>('addHero'))
     );
   }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hotel id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+        this.log(`found hotels matching "${term}"`) :
+        this.log(`no hotel matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
 }
